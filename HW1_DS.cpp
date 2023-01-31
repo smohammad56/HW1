e28beb0158389a54343d2ebfe227f5c03a8e1a84
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2023-01-31 17:11:59 -0500

    Implemented Letter Frequency and counts

--- HW1_DS.cpp
+++ HW1_DS.cpp
@@ -2,42 +2,44 @@
 #include<string>
 #include<fstream>
 #include<iostream>
-using namespace std;
 
 
 struct Book {
-	string title;
-	string first_name;
-	string last_name;
+	std::string title;
+	std::string first_name;
+	std::string last_name;
 	int word_count;
-	double frequency;
+	float frequency[26];
 	int line_count;
 
 };
 
 int main() {
-	string filename;
-	string line;
-	ifstream file;
-
-	//This is a test comment
-	int test = 0;
+	std::string filename;
+	std::string line;
+	std::ifstream file;
 
 	do {
-		cout << "Enter filename: ";
-		cin >> filename;
+		std::cout << "Enter filename: ";
+		std::cin >> filename;
 		file.open(filename);
 	} while (file.fail());
 
 	int i = 0;
+	int space_count = 0;
+	int char_count = 0;
 
 	struct Book b1;
-	int letter_array[26];
+	for (int i = 0; i < 26; i++)
+	{
+		b1.frequency[i] = 0;
+	}
+
 
 	while (!(file.eof())) {
 
 		getline(file, line);
-		cout << line << endl;
+		std::cout << line << std::endl;
 
 		if (i == 0) {
 			b1.title = line;
@@ -57,21 +59,40 @@ int main() {
 				}
 			}
 			b1.last_name.erase(0,1);
+			/*b1.last_name = line.substr(0, line.find(' '));
+			b1.first_name = line.substr(line.find(' '), line.length());*/
 		}
 		else if (line != "Contents:"){
-			for (int i = 0; i < line.size(); i++) {
-				letter_array[int(line[i]) - 97] += 1;
-
-
+			for (int j = 0; j < line.size(); j++) {
+				if (int(line[j]) >= 97) 
+					b1.frequency[int(line[j]) - 97] += 1;
+				else
+					b1.frequency[int(line[j]) - 65] += 1;
+				if (line[j] == ' ')
+					space_count++;
+				char_count++;
 			}
-
 		}
 		i++;
 	}
+	b1.line_count = i - 3;
+	b1.word_count = i + space_count;
+	for (i = 0; i < 26; i++)
+	{
+		b1.frequency[i] /= char_count;
+		b1.frequency[i] *= 100;
+	}
 
-	cout << b1.title;
-	cout << b1.first_name;
-	cout << b1.last_name;
-	cout << letter_array;
+	std::cout << "Title: " << b1.title << std::endl;
+	std::cout << "First Name: " << b1.first_name << std::endl;
+	std::cout << "Last Name: " << b1.last_name << std::endl;
+	std::cout << "Line Count: " << b1.line_count << std::endl;
+	std::cout << "Word Count: " << b1.word_count << std::endl;
+	for (i = 0; i < 26; i++)
+	{
+		std::cout << char(i + 97) << ": " << b1.frequency[i] << "%" << std::endl;
+	}
+
+	file.close();
 
 }
