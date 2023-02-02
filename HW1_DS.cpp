8ceb4a0bd10f763f4755cfd93009bb3d8bc14eff
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2023-02-01 19:06:12 -0500

    Fixed array overflow error and others

--- HW1_DS.cpp
+++ HW1_DS.cpp
@@ -6,11 +6,12 @@
 
 struct Book {
 	std::string title;
+	std::string full_name;
 	std::string first_name;
 	std::string last_name;
 	int word_count;
-	float frequency[26];
 	int line_count;
+	float frequency[26];
 
 };
 
@@ -24,78 +25,111 @@ int main() {
 		std::cin >> filename;
 		file.open(filename);
 	} while (file.fail());
-
-	int i = 0;
-	int space_count = 0;
+ 
 	int char_count = 0;
 	int char_value = 0;
 
+
+
+	std::string full_name;
+	std::string first_name;
+	std::string last_name;
+	std::string full_name1 = "";
+	std::string first_name1 = "";
+	std::string last_name1 = "";
+
+
+
 	struct Book b1;
+	b1.word_count = 0;
+	b1.line_count = 0;
 	for (int i = 0; i < 26; i++)
 	{
 		b1.frequency[i] = 0;
 	}
-
 
 	while (!(file.eof())) {
 
 		getline(file, line);
 		std::cout << line << std::endl;
 
-		if (i == 0) {
+		if (b1.line_count == 0) {
 			b1.title = line;
 		}
-		else if (i == 1) {
+		else if (b1.line_count == 1) {
 			int flag = 0;
-			for (int i = 0; i < line.size(); i++) {
-				if (line[i] == ' ') {
+			full_name1 = line;
+			for (int k = 0; k < line.size(); k++) {
+				if (line[k] == ' ') {
 					flag = 1;
 				}
 				if (flag == 1) {
-					b1.last_name += line[i];
+					last_name1 += line[k];
 				}
 				if (flag == 0)
 				{
-					b1.first_name += line[i];
+					first_name1 += line[k];
 				}
 			}
-			b1.last_name.erase(0,1);
+			last_name1.erase(0,1);
+
+			full_name = full_name1;
+			first_name = first_name1;
+			last_name = last_name1;
 			/*b1.last_name = line.substr(0, line.find(' '));
 			b1.first_name = line.substr(line.find(' '), line.length());*/
 		}
 		else if (line != "Contents:"){
 			for (int j = 0; j < line.size(); j++) {
 				if (int(line[j]) >= 97)
+				{
 					char_value = int(line[j]) - 97;
+				}
 				else
+				{
 					char_value = int(line[j]) - 65;
-				if (line[j] == ' ')
-					space_count++;
-				else if (char_value >= 0 || char_value <= 25)
+				}
+				if (line[j] == ' ' || line[j] == '\n')
+				{
+					b1.word_count++;
+				}
+				else if (char_value >= 0 && char_value <= 25)
+				{
 					char_count++;
 					b1.frequency[char_value] += 1;
+				}
 			}
 		}
-		i++;
+		b1.line_count++;
 	}
-	b1.line_count = i - 3;
-	b1.word_count = i + space_count;
-	for (i = 0; i < 26; i++)
+	file.close();
+
+
+	b1.line_count -= 3;
+	//b1.full_name = b1.first_name + " " + b1.last_name;
+	//memset(&(b1.full_name), 0, sizeof(b1.full_name));
+	b1.full_name = full_name;
+	b1.first_name = first_name;
+	b1.last_name = last_name;
+
+	for (int i = 0; i < 26; i++)
 	{
+		std::cout << char(i + 97) << ": " << b1.frequency[i] << std::endl;
 		b1.frequency[i] /= char_count;
 		b1.frequency[i] *= 100;
 	}
 
 	std::cout << "Title: " << b1.title << std::endl;
+	std::cout << "Full Name: " << b1.full_name << std::endl;
 	std::cout << "First Name: " << b1.first_name << std::endl;
 	std::cout << "Last Name: " << b1.last_name << std::endl;
 	std::cout << "Line Count: " << b1.line_count << std::endl;
 	std::cout << "Word Count: " << b1.word_count << std::endl;
-	for (i = 0; i < 26; i++)
+	std::cout << "Character Count: " << char_count << std::endl;
+	for (int i = 0; i < 26; i++)
 	{
 		std::cout << char(i + 97) << ": " << b1.frequency[i] << "%" << std::endl;
 	}
 
-	file.close();
-
+	return 0;
 }
